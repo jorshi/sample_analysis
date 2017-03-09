dcb52829abd0fdd4e86fab1539e771464dba36cf
jorshi <jordieshier@gmail.com>
2017-03-08 22:22:10 -0800

    Cleaned up remove_outlier command

--- sample_analysis/analysis/management/commands/remove_outliers.py
+++ sample_analysis/analysis/management/commands/remove_outliers.py
@@ -1,20 +1,37 @@
-import sys
-import essentia.standard as es
+"""
+Command for determining outliers from analysis and marking
+them so they can be left out from further analysis. All analysis
+features are first normalized and then the average distance to
+the three nearest neighbours is used to determine whether a sample
+is an outlier. If the distance is above a set threshold then that
+sample is marked as an outlier.
+
+    Usage: python ./manage.py remove_outliers sample_type
+"""
+
 from sklearn.neighbors import NearestNeighbors
 from sklearn import preprocessing
 import numpy as np
-import math
 from django.core.management.base import BaseCommand, CommandError
-from django.db.models import Avg
-from analysis.models import Sample, Analysis, AnalysisRobustScale 
+from analysis.models import Sample, Analysis
+
 
 class Command(BaseCommand):
     help = 'Mark Outliers'
-    
+
+    # Distance threshold for determining an outlier
+    THRESH = 2.5
+
+    # Override of the add_argument method
+    def add_arguments(self, parser):
+
+        # Required argument for the type of analysis to run
+        parser.add_argument('sample_type', nargs=1, type=str)
+
 
     # Executes on command runtime
     def handle(self, *args, **options):
-        
+
         # Dimensions to consider in outlier calculation
         dimensions = [
             'duration',
@@ -30,44 +47,46 @@ class Command(BaseCommand):
 
         # Get all the samples
         analysisObjects = Analysis.objects.filter(
-            sample__sample_type='sn', 
+            sample__sample_type=options['sample_type'][0],
             sample__exclude=False,
             sample__kit__sample_pack__exclude=False,
         )
         matrix = []
         order = []
-        sampleOrder = []
 
-        # Create a numpy matrix of the analyis
+        # Create a numpy matrix of the analyis objects
         for item in analysisObjects:
             order.append(item.id)
-            sampleOrder.append(item.sample)
             matrix.append([getattr(item, d) for d in dimensions])
 
         nOrder = np.array(order)
         nMatrix = np.array(matrix)
-        
-        # Scale erything
-        scaled = preprocessing.scale(nMatrix)
-        nMatrix = np.array(scaled)
+
+        # Scale everything
+        nMatrix = preprocessing.scale(nMatrix)
 
         # Remove outliers based on nearest neighbor calulcations
         nbrs = NearestNeighbors(n_neighbors=4, algorithm='ball_tree').fit(nMatrix)
         distances, indices = nbrs.kneighbors(nMatrix)
 
+        # Average distance of three nearest neighbours. The first neighbour is the
+        # sample itself which will always have a distance of zero. Not including
+        # that in the average
         averageDistance = []
         for d in distances:
             averageDistance.append(np.average(d[1:]))
-        
-        x = np.array(averageDistance)
-        condList = [x > 2.5]
-        choice = [x]
 
+        # Determine outliers and save the ids of the analysis objects to an array
+        x = np.array(averageDistance)
+        condList = [x > self.THRESH]
+        choice = [x]
         rejects = np.select(condList, choice)
         rejectSamples = nOrder[np.nonzero(rejects)]
 
-        sampleIds = []
+        # Mark outliers and update in DB
         for analysisId in rejectSamples:
             analysisObject = Analysis.objects.get(id=analysisId)
             analysisObject.outlier = True
             analysisObject.save()
+
+        print "%s outlier(s) found and marked" % len(rejectSamples)
