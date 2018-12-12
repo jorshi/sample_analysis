3dfbbb7c38d9ee751ad1e71e65e6731208971dc0
jorshi <jordieshier@gmail.com>
2018-12-12 14:15:47 -0800

    Deleting a bunch of old files and updating models to the necassary ones

--- sample_analysis/analysis/admin.py
+++ sample_analysis/analysis/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from .models import SamplePack, Kit, Sample, Analysis, Tag, Manufacturer
+from .models import SamplePack, Kit, Sample, Tag, Manufacturer
 
 admin.site.register(SamplePack)
 admin.site.register(Kit)
 admin.site.register(Sample)
-admin.site.register(Analysis)
 admin.site.register(Tag)
 admin.site.register(Manufacturer)
 
